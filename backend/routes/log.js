9970b2fddee72e16d24e56a358149b73bc71f31f
Draculakl07 <avinashbabu068@gmail.com>
2025-05-10 19:47:06 +1000

    Styled mobile UI with Bootstrap

--- backend/routes/log.js
+++ backend/routes/log.js
@@ -2,14 +2,20 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const logFile = path.join(__dirname, '../admin-log.txt');
 
 router.post('/', (req, res) => {
   const { action, timestamp } = req.body;
-  const logPath = path.join(__dirname, '../admin-log.txt');
-
-  fs.appendFile(logPath, `[${timestamp}] ${action}\n`, err => {
+  fs.appendFile(logFile, `[${timestamp}] ${action}\n`, err => {
     if (err) return res.status(500).send('Log failed');
     res.send('Logged');
+  });
+});
+
+router.get('/', (req, res) => {
+  fs.readFile(logFile, 'utf8', (err, data) => {
+    if (err) return res.status(500).send('Could not load logs');
+    res.send(data);
   });
 });
 
